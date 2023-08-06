82ad669d0d80d8eb976e3515d5bf9bcbe5ae81ba
sheegull <sheegulls@gmail.com>
2023-08-07 01:08:39 +0900

    💄 update request.js

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -1,15 +1,12 @@
-import "dotenv/config";
-require("dotenv").config();
-
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-us`,
-  feachNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-  feactTopRated: `/discover/tv?api_key=${API_KEY}&languager=en-us`,
-  feactActionMovies: `/discover/tv?api_key=${API_KEY}&with_genres=28`,
-  feactComedyMovies: `/discover/tv?api_key=${API_KEY}&with_genres=35`,
-  feactHorrorMovies: `/discover/tv?api_key=${API_KEY}&with_genres=27`,
-  feactRomanceMovies: `/discover/tv?api_key=${API_KEY}&with_genres=10749`,
-  feactDocumentMovies: `/discover/tv?api_key=${API_KEY}&with_genres=99`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/discover/tv?api_key=${API_KEY}&language=en-us`,
+  fetchActionMovies: `/discover/tv?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/tv?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/tv?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/tv?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentMovies: `/discover/tv?api_key=${API_KEY}&with_genres=99`,
 };
