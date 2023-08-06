250e5df785d225362e1992fdb477ae3c264b8355
sheegull <sheegulls@gmail.com>
2023-08-07 04:14:35 +0900

    🐛 fix bug

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -4,9 +4,9 @@ export const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-us`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchTopRated: `/discover/tv?api_key=${API_KEY}&language=en-us`,
-  fetchActionMovies: `/discover/tv?api_key=${API_KEY}&with_genres=28`,
-  fetchComedyMovies: `/discover/tv?api_key=${API_KEY}&with_genres=35`,
-  fetchHorrorMovies: `/discover/tv?api_key=${API_KEY}&with_genres=27`,
+  fetchActionMovies: `/discover/tv?api_key=${API_KEY}&with_genres=35`,
+  fetchComedyMovies: `/discover/tv?api_key=${API_KEY}&with_genres=36`,
+  fetchHorrorMovies: `/discover/tv?api_key=${API_KEY}&with_genres=37`,
   fetchRomanceMovies: `/discover/tv?api_key=${API_KEY}&with_genres=10749`,
   fetchDocumentMovies: `/discover/tv?api_key=${API_KEY}&with_genres=99`,
 };
